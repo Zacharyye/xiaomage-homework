4a5849724ab162d4904cba202f38295ae1e2ea33
zachary <zhouyin1994@hotmail.com>
2021-07-07 16:18:57 +0800

    complete the homework

--- week01/homework/src/main/java/com/zachary/homework/tags/CustomResponseHeadersTag.java
+++ week01/homework/src/main/java/com/zachary/homework/tags/CustomResponseHeadersTag.java
@@ -1,22 +1,57 @@
 package com.zachary.homework.tags;
 
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
+import org.springframework.util.StringUtils;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspContext;
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 import java.io.IOException;
 
 public class CustomResponseHeadersTag extends SimpleTagSupport {
+  private String cacheControl;
+  private String pragma;
+  private long expires;
 
   @Override
   public void doTag() throws JspException, IOException {
-//    super.doTag();
-    ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-    HttpServletResponse response = attributes.getResponse();
-    response.setHeader("Cache-Control", "no-cache");
-    response.setHeader("Pragma", "no-cache");
-    response.setDateHeader("Expires", -1);
+    super.doTag();
+    getJspContext();
+    System.out.println(getJspContext());
+    JspContext jspContext = getJspContext();
+    PageContext pageContext = (PageContext) jspContext;
+    HttpServletResponse response = (HttpServletResponse) pageContext.getResponse();
+    if(StringUtils.hasText(cacheControl)) {
+      response.setHeader("Cache-Control", cacheControl);
+    }
+    if(StringUtils.hasText(pragma)) {
+      response.setHeader("Pragma", pragma);
+    }
+    response.setDateHeader("Expires", expires);
   }
+
+  public String getCacheControl() {
+    return cacheControl;
+  }
+
+  public void setCacheControl(String cacheControl) {
+    this.cacheControl = cacheControl;
+  }
+
+  public String getPragma() {
+    return pragma;
+  }
+
+  public void setPragma(String pragma) {
+    this.pragma = pragma;
+  }
+
+  public long getExpires() {
+    return expires;
+  }
+
+  public void setExpires(long expires) {
+    this.expires = expires;
+  }
+
 }
--- week01/homework/src/main/webapp/index.jsp
+++ week01/homework/src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
+
+<%@ taglib uri="/WEB-INF/custom-tags.tld" prefix="sm" %>
+<sm:common-response-headers cacheControl="no-cache" expires="-2" pragma="no-cache" />
 <html>
 <head>
     <title>JSP - Hello World</title>
